996bbeb8b63757c5b0a5b9482578b2707ef7bddc
mai.shu <ansiboy@163.com>
2020-04-10 20:26:00 +0800

    content 方法支持 headers

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+function modifyVersion() {
+    const package = require("./package.json");
+
+    let version = package.version || "1.0.0";
+    let arr = version.split(".");
+    arr[arr.length - 1] = (Number.parseInt(arr[arr.length - 1]) + 1).toString();
+    version = arr.join(".");
+    package.version = version;
+
+    const fs = require('fs');
+    let data = JSON.stringify(package, null, 4);
+    fs.writeFileSync("package.json", data, "utf8");
+};
+modifyVersion();
+
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        shell: {
+            src: {
+                command: `tsc -p src`
+            }
+        },
+    });
+
+    grunt.registerTask('default', ['shell']);
+}
--- src/controller.ts
+++ src/controller.ts
@@ -1,18 +1,27 @@
 import { ContentResult, RedirectResult, ProxyResut, contentTypes } from "./action-results";
 
+type Headers = { [key: string]: string | string[] };
 export class Controller {
 
     static typeName = "node-mvc.Controller";
 
-    content(value: string, statusCode: number): ContentResult;
-    content(value: string, type: string, statusCode?: number): ContentResult;
-    content(value: string, type?: string | number, statusCode?: number): ContentResult {
+    content(value: string, statusCode?: number): ContentResult;
+    content(value: string, contentType: string, statusCode?: number): ContentResult;
+    content(value: string, headers: Headers, statusCode?: number): ContentResult;
+    content(value: string, type?: string | number | Headers, statusCode?: number): ContentResult {
         if (typeof type == "number") {
             statusCode = type;
             type = undefined;
         }
-        let r = new ContentResult(value, type, statusCode);
-        return r
+
+        let r: ContentResult;
+        if (typeof type == "number") {
+            r = new ContentResult(value, type);
+        }
+        else {
+            r = new ContentResult(value, type as any, statusCode);
+        }
+        return r;
     }
     json(obj: any, statusCode?: number) {
         let str = JSON.stringify(obj)
