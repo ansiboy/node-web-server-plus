cd07e17924b6607f7e0fdd67661432a77f79c061
mai.shu <ansiboy@163.com>
2020-12-20 12:22:45 +0800

    实现插件

---
+++ src/load-plugins.ts
@@ -0,0 +1,50 @@
+import { Logger } from "log4js";
+import { VirtualDirectory, WebServer } from "maishu-node-web-server";
+import * as path from "path";
+
+export function loadPlugins(rootDirectory: VirtualDirectory, logger: Logger, webServer: WebServer) {
+
+    let nodeModulesDir = rootDirectory.findDirectory("node_modules");
+    if (nodeModulesDir == null) {
+        logger.warn(`Node_modules directory is not exists in the root directory '${rootDirectory.physicalPath}'.`)
+        return;
+    }
+
+    let pluginNameRegex = new RegExp("\\S+-nws-")
+    let dirs = nodeModulesDir.directories();
+    for (let name in dirs) {
+        if (!pluginNameRegex.test(name)) {
+            continue;
+        }
+
+        let packagePhysicalPath = dirs[name].findFile("package.json");
+        if (!packagePhysicalPath) {
+            logger.warn(`File package.json is not exists in the directory ${packagePhysicalPath}.`);
+            continue;
+        }
+
+        let pkg = require(packagePhysicalPath);
+        let mainPath = pkg.main || "index.js";
+        if (!path.isAbsolute(mainPath))
+            mainPath = path.join(dirs[name].physicalPath, mainPath);
+
+        let mod = require(mainPath);
+        if (mod == null) {
+            logger.warn(`Load package '${pkg.name}' fail.`);
+            continue;
+        }
+
+        if (mod.default == null) {
+            logger.warn(`Package '${pkg.name}' has not a default field, is has a default export?.`);
+            continue;
+        }
+
+        if (typeof mod.default != "function") {
+            logger.warn(`Package '${pkg.name}' default export is not a function, require a function.`);
+            continue;
+        }
+
+        mod.default(webServer);
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { Settings } from "./types";
 import {
-    WebServer, StaticFileProcessor, HeadersProcessor, VirtualDirectory
+    WebServer, StaticFileProcessor, HeadersProcessor, VirtualDirectory,
+    getLogger
 } from "maishu-node-web-server";
 
 import { MVCRequestProcessor } from "maishu-node-web-server-mvc";
@@ -9,6 +10,8 @@ import { Json5Processor } from "./processors/json5-processor";
 import { LessProcessor } from "./processors/less-processor";
 import * as errors from "./errors";
 import * as fs from "fs";
+import * as path from "path";
+import { loadPlugins } from "./load-plugins";
 
 export function startServer(settings: Settings) {
 
@@ -34,37 +37,35 @@ export function startServer(settings: Settings) {
 
     let server = new WebServer(settings);
 
-    let staticFileProcessor = server.requestProcessors.filter(o => o instanceof StaticFileProcessor)[0];
-    console.assert(staticFileProcessor != null, "Can not find static file processor");
-    let staticFileProcessorIndex = server.requestProcessors.indexOf(staticFileProcessor);
+    // let staticFileProcessor = server.requestProcessors.find(StaticFileProcessor);
+    // console.assert(staticFileProcessor != null, "Can not find static file processor");
+    // let staticFileProcessorIndex = server.requestProcessors.add(staticFileProcessor);
 
     var javaScriptProcessor = new JavaScriptProcessor();
-    server.requestProcessors.splice(staticFileProcessorIndex, 0, javaScriptProcessor);
+    server.requestProcessors.add(javaScriptProcessor);
 
     var json5Processor = new Json5Processor();
-    server.requestProcessors.splice(staticFileProcessorIndex, 0, json5Processor);
+    server.requestProcessors.add(json5Processor);
 
     var lessProcessor = new LessProcessor();
-    server.requestProcessors.splice(staticFileProcessorIndex, 0, lessProcessor);
+    server.requestProcessors.add(lessProcessor);
 
     if (settings.controllerDirectory) {
         let mvcProcessor = new MVCRequestProcessor({
             controllersDirectory: settings.controllerDirectory,
             serverContextData: settings.serverContextData,
         });
-        server.requestProcessors.splice(staticFileProcessorIndex, 0, mvcProcessor);
+        server.requestProcessors.add(mvcProcessor);
     }
 
     if (settings.headers) {
-        var headersProcessor = server.requestProcessors.filter(o => o instanceof HeadersProcessor)[0] as HeadersProcessor;
+        var headersProcessor = server.requestProcessors.find(HeadersProcessor);
         console.assert(headersProcessor != null, "Can not find headers processor.");
         headersProcessor.headers = headersProcessor.headers || {};
         for (let key in settings.headers) {
             headersProcessor.headers[key] = settings.headers[key];
         }
     }
-
-
 
     if (settings.virtualPaths) {
         for (let virtualPath in settings.virtualPaths) {
@@ -75,6 +76,12 @@ export function startServer(settings: Settings) {
             server.websiteDirectory.setPath(virtualPath, physicalPath);
         }
     }
+
+    let packagePath = "../package.json";
+    let pkg = require(packagePath);
+    let logger = getLogger(pkg.name, settings.log?.level)
+    loadPlugins(rootDirectory, logger, server);
+
 
     return server;
 }
