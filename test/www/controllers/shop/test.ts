5365671e3037fb66025698c42720cbb03b9e973d
mai.shu <ansiboy@163.com>
2020-12-21 20:21:13 +0800

    移除 mvc test

--- test/www/controllers/shop/test.ts
+++
@@ -1,8 +0,0 @@
-import { controller, action } from '../../../../out/'
-@controller()
-export default class TestController {
-    @action("/shop/test")
-    index() {
-        return "shop test index"
-    }
-}
