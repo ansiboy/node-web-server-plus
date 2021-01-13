1185d82ab0359f9a9bddf260d470431fb38a1066
mai.shu <ansiboy@163.com>
2021-01-13 15:00:47 +0800

    support json file type

--- src/processors/json5-processor.ts
+++ src/processors/json5-processor.ts
@@ -2,6 +2,7 @@ import { RequestContext, RequestProcessor, RequestResult } from "maishu-node-web
 import * as errors from "../errors";
 import * as fs from "fs";
 import * as JSON5 from "json5";
+import * as path from "path";
 
 export class Json5Processor implements RequestProcessor {
     async execute(ctx: RequestContext): Promise<RequestResult | null> {
@@ -9,9 +10,18 @@ export class Json5Processor implements RequestProcessor {
         if (!virtualPath.endsWith(".json") && !virtualPath.endsWith(".json5"))
             return null;
 
-        let physicalPath = ctx.rootDirectory.findFile(virtualPath);
+        let extname = path.extname(ctx.virtualPath);
+        let fileName = ctx.virtualPath.substr(0, ctx.virtualPath.length - extname.length);
+
+        let jsonFileName = fileName + ".json";
+        let json5FileName = fileName + ".json5";
+
+        let physicalPath = ctx.rootDirectory.findFile(jsonFileName);
         if (physicalPath == null)
-            throw errors.pageNotFound(virtualPath);
+            physicalPath = ctx.rootDirectory.findFile(json5FileName);
+
+        if (physicalPath == null)
+            throw errors.pageNotFound(`${jsonFileName} or ${json5FileName}`);
 
         let buffer = fs.readFileSync(physicalPath);
         let obj = JSON5.parse(buffer.toString());
