c71c3f0a77b543b61a30d1415888062114782ebb
mai.shu <ansiboy@163.com>
2021-05-20 10:46:05 +0800

    添加 less 输出路径

--- src/processors/less-processor.ts
+++ src/processors/less-processor.ts
@@ -61,6 +61,7 @@ export class LessProcessor implements RequestProcessor {
                 content = originalCode;
         }
 
+        content = `Physical Path: ${physicalPath} \r\n${content}`;
 
         return {
             content: content,
